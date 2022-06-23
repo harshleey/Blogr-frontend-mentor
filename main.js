754b5738bb1708d831924f9d1b6ad64b60a2abd6
Adekunle Fatima <logun.fateem@gmail.com>
2022-06-23 12:41:01 +0100

    Added a new section

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-let product = document.querySelector(".product")
+let product = document.querySelector(".products")
 let innerLinkContainer = document.querySelector(".inner-link-container")
+let navToggle = document.querySelector(".nav-toggle")
+let menuBox = document.querySelector(".menu-box")
 
-
-product.addEventListener('click', function() {
- innerLinkContainer.classList.toggle('inner-link-container.active')
- console.log("I am a winner")
+navToggle.addEventListener('click', function() {
+ menuBox.classList.toggle("show-menu-box");
+ // console.log("I am a winner")
  // if (innerLinks.classList.contains(''))
- // innerLinkContainer.style.display = "block"
+ // innerLinkContainer.style.display.toggle = "block" 
 })
+
